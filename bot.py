38ccc727218015d041284078eb980105707aa9dc
DoctorFoxy <mmedaer@gmail.com>
2018-06-10 12:05:27 +0200

    Edit bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,28 @@
+______           _            ______              
+|  _  \         | |           |  ___|             
+| | | |___   ___| |_ ___  _ __| |_ _____  ___   _ 
+| | | / _ \ / __| __/ _ \| '__|  _/ _ \ \/ / | | |
+| |/ / (_) | (__| || (_) | |  | || (_) >  <| |_| |
+|___/ \___/ \___|\__\___/|_|  \_| \___/_/\_\\__, |
+                                             __/ |
+                                            |___/ 
+
 import discord 
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 
 client = discord.Client()
-client = commands.Bot(command_prefix = "=")
+client = Bot(description="Striker bot by DoctorFoxy", command_prefix="=", pm_help = False)
 
 @client.event
 async def on_ready():
 	print("Bot is ready!")
+	print("----------------------")
+	print("Striker is now running.")
+	print("----------------------")
+	print("Made by DoctorFoxy.")
+	print("======================")
 	
 @client.event
 async def on_message(message):
@@ -17,3 +31,15 @@ async def on_message(message):
 
 
 client.run("NDU1MTExODQzNjE5NDA1ODM1.Df6Epw._emD_LSqM7Bi8JB_iF2rWsnDNf0")
+
+
+
+
+
+
+
+
+
+
+
+
